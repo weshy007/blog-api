3153f20a87cd401bd253460cbc57c9aa7a200928
weshy <josephwaweru96@gmail.com>
2022-05-27 19:13:39 +0300

    endpoints

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("posts.urls")),
+    path('api/v1/', include("posts.urls")),
     
 ]
